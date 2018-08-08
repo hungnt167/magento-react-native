13e3e8570f76def209127c7a11ba62d386b4e664
Dmytro Portenko <troublesohard@gmail.com>
2018-08-08 17:36:54 +0300

    add post clone script for app center

---
+++ ios/appcenter-post-clone.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "
+export const magentoOptions = {
+    url: '$BASE_URL',
+    authentication: {
+      integration: {
+        access_token: '$ACCESS_TOKEN'
+      }
+    }
+};
+" > ../src/config/magento.js
